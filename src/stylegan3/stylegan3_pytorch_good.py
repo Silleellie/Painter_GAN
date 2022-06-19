a3d78b38053d2ef88335405c75baee9ef16fd949
Silleellie <antonio.silletti@gmail.com>
2022-06-19 11:54:06 +0200

    fix error directory stylegan3 repo

--- src/stylegan3/stylegan3_pytorch_good.py
+++ src/stylegan3/stylegan3_pytorch_good.py
@@ -1,15 +1,19 @@
 import os
+import pathlib
 
 
-current_path = os.path.realpath(__file__)
+current_path = pathlib.Path(__file__).parent.resolve()
 stylegan3_repo = os.path.join(current_path, 'stylegan3_repo')
 
 
 class StyleGAN3:
 
     def prepare_dataset(self, monet_dataset_source: str, monet_dataset_output: str, res: int = 256):
-        comm = f"python {stylegan3_repo}/dataset_tool.py --source={monet_dataset_source} --dest={monet_dataset_output} " \
-               f"--resolution='{res}x{res}'"
+
+        dataset_tool = os.path.join(stylegan3_repo, 'dataset_tool.py')
+
+        comm = f"python {dataset_tool} --source={monet_dataset_source} --dest={monet_dataset_output} " \
+               f"--resolution={res}x{res}"
 
         os.system(comm)
 
@@ -17,7 +21,9 @@ class StyleGAN3:
               batch_size: int = 32, batch_size_gpu: int = 16, gamma_value: int = 2, snapshot_count: int = 5,
               kimg: int = 100, workers: int = 2):
 
-        comm = f"python {stylegan3_repo}/train.py --outdir='{output_dir}' --tick=2 --workers={workers} --mirror=1 " \
+        train_module = os.path.join(stylegan3_repo, 'train.py')
+
+        comm = f"python {train_module} --outdir='{output_dir}' --tick=2 --workers={workers} --mirror=1 " \
                f"--cfg=stylegan3-t --data={dataset_path} --gpus=1 --batch={batch_size} --batch-gpu={batch_size_gpu} " \
                f"--gamma={gamma_value} --snap={snapshot_count} --resume={resume_from} --kimg={kimg} " \
                "--freezed=10 --metrics=None "
